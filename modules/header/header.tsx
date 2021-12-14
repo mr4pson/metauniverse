2fd575a1b84b72f43279f8823f69344dd997900d
GoryachkinV <GoryachkinV>
2021-12-15 00:34:10 +0300

    add website header

---
+++ modules/header/header.tsx
@@ -0,0 +1,47 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import { headerLinks } from './constants';
+import styles from './header.module.scss';
+import { useRouter } from 'next/router';
+import classNames from 'classnames';
+
+const Header = (): JSX.Element => {
+  const router = useRouter();
+
+  return (
+    <div className={styles["header-wrapper"]}>
+      <div className={styles["header"]}>
+        <div className={styles["header__logo-links-wrapper"]}>
+          <div className={styles['header__logo']} />
+          <div className={styles['header__links']}>
+            {
+              headerLinks.map(linkGroup => (
+                <div className={styles['header__link-group']}>
+                  <Link href={linkGroup.link}>
+                    <span className={classNames(
+                      styles['header__link'], 
+                      { [styles['header__link_active']]: router.pathname === linkGroup.link }
+                      )}
+                    >
+                      {linkGroup.title}
+                    </span>
+                  </Link>
+                  {
+                    linkGroup.isDividerExist && (<div className={
+                    classNames(
+                      styles['vertical-divider'], 
+                      { [styles['vertical-divider_active']]: router.pathname === linkGroup.link }
+                    )} 
+                  />)
+                  }
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
