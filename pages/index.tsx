e56f384f1b3db30ca44e9b91fe82fa951a920b3a
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2021-12-16 01:50:49 +0300

    created introduction component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import { Header, PageTitle } from 'modules';
 import Head from 'next/head';
-import { RightArrowIcon } from 'public/icons';
-import { Button } from 'ui-kit';
 import styles from './index.module.scss';
+import IntroductionSection from '../components/introduction-section';
 
 const IndexPage = (): JSX.Element => {
   return (
@@ -16,28 +15,7 @@ const IndexPage = (): JSX.Element => {
       </Head>
       <Header />
       <div className={styles['page-wrapper']}>
-        <div className={styles["index-page"]}>
-          <div className={styles["page-title"]}>
-            <PageTitle className={styles["page-title__top"]} normalText={"What's a"} wrappedText={"Dendy"} />
-            <div className={styles["page-title__bottom"]}>Metaverse?</div>
-          </div>
-          <div className={styles['page-description']}>
-            The Dendy Metaverse is an innovative DeFi game platform based on the Play to Earn concept. 
-            With us you can play the most popular seven 
-            Dendy games straight from your childhood.
-            <br />We also have Dendy Swap and Dandy Launchpad!
-            It's never been easier to earn!
-          </div>
-          <div className={styles['footer']}>
-            <Button
-              icon={
-                <RightArrowIcon />
-              }
-            >
-              White Paper
-            </Button>
-          </div>
-        </div>
+        <IntroductionSection />
       </div>
     </>
   );
