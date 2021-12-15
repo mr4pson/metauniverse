597a8fbdfe7b3248051bbd18dcefd6d428c8ee47
GoryachkinV <GoryachkinV>
2021-12-15 04:02:19 +0300

    added index page bottom part

---
+++ public/icons/right-arrow-icon.tsx
@@ -0,0 +1,20 @@
+import { TypeIcon } from './types';
+
+export const RightArrowIcon = (): JSX.Element => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="21" viewBox="0 0 14 21" fill="none">
+  <g filter="url(#filter0_d_0_95)">
+  <path d="M3.03555 3.42893L10.1066 10.5L3.03555 17.5711" stroke="#02FFB9" stroke-width="2"/>
+  </g>
+  <defs>
+  <filter id="filter0_d_0_95" x="0.328491" y="0.721802" width="13.1924" height="19.5564" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+  <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+  <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+  <feOffset/>
+  <feGaussianBlur stdDeviation="1"/>
+  <feColorMatrix type="matrix" values="0 0 0 0 0.00784314 0 0 0 0 1 0 0 0 0 0.72549 0 0 0 1 0"/>
+  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_0_95"/>
+  <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_0_95" result="shape"/>
+  </filter>
+  </defs>
+  </svg>
+);
